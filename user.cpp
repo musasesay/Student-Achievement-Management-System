757ba6a3930c514beea13e1177f7ab56dc533206
ptspzy <1126563580@qq.com>
2015-05-11 09:12:31 +0800

    主函数

---
+++ user.cpp
@@ -0,0 +1,33 @@
+/*
+Title：Student Achievement Management System
+Time：2015-05-10
+*/
+#include<iostream>
+#include"student.h"
+int main()
+{
+	int x, i = 0;
+	bool quit = false;
+	cout << endl << endl;
+	cout << "\t\tWelcome To Student Achievement Management System." << endl << endl << endl;
+	Studentinfo Grade;
+	cout << "\t\t按任意键开始……";
+	getchar();
+	while (!quit)
+	{
+		system("cls");
+		Grade.ShowMenu();
+		cin >> x;
+		switch (x)
+		{
+		case 0:quit = true; break;
+		case 1:Grade.AddItem(); break;
+		case 2:Grade.Display(); break;
+		case 3:Grade.Sort(); break;
+		case 4:Grade.Find(); break;
+		case 5:Grade.RemoveItem(); break;
+		case 6:Grade.ModifyItem(); break;
+		}
+	}
+	return 0;
+}
